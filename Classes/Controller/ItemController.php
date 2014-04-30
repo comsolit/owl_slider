bb097fa87fc393c4c7f37fe4ffa223872ece6f31
Andres <lobacovs@comsolit.ch>
2014-04-30 17:13:51 +0200

    translations, imageHeight and customNav accessable from backend

--- Classes/Controller/ItemController.php
+++ Classes/Controller/ItemController.php
@@ -50,7 +50,7 @@ class ItemController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController 
 	public function listAction() {
 		$items = $this->itemRepository->findAll();
 		$this->view->assign('items', $items);
-		$this->view->assign('configuration', $this->configuration);
+// 		$this->view->assign('configuration', $this->configuration);
 	}
 
 	/**
