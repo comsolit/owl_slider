0f09f7eac21d83feb7b136e172e67416a299a272
Lars Messmer <messmer@comsolit.ch>
2014-05-07 18:24:52 +0200

    [task] set version and dependencies for TYPO3

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,12 +27,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.1.0',
+	'version' => '1.1.1',
 	'constraints' => array(
 		'depends' => array(
 			'extbase' => '6.0',
 			'fluid' => '6.0',
 			'typo3' => '6.0.99-6.2.99',
+			'css_styled_content' => ''
+			
 		),
 		'conflicts' => array(
 		),
