cbd8a3b13b7daacf61dc7a331106779612119287
Philipp Kitzberger <pk@cron.eu>
2017-09-14 11:29:45 +0200

    TYPO3 8.7 constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,9 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '2.4.2',
     'constraints' => array(
         'depends' => array(
-            'extbase' => '6.0.99-7.6.99',
-            'fluid' => '6.0.99-7.6.99',
-            'typo3' => '6.0.99-7.6.99',
+            'typo3' => '8.7.0-8.7.99',
         ),
         'conflicts' => array(),
         'suggests' => array()
