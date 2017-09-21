8ccd3683a0a626d291c6cb2f4361c63362388a69
Philipp Kitzberger <philipp@kitze.net>
2017-09-21 08:43:18 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '2.4.2',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '6.2.0-8.7.99',
         ),
         'conflicts' => array(),
         'suggests' => array()
