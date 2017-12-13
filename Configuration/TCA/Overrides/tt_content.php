fa273f301290c83c13e753c7610ce0e8b966953a
Maurizio Silipo <silipo@comsolit.com>
2017-12-13 16:50:12 +0100

    override sys_template and tt_content instead of writing tca globals directly in ext_tables.php

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,20 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+$extKey = 'owl_slider';
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    $extKey,
+    'Owlslider',
+    'owlSlider'
+);
+
+$pluginSignature = str_replace('_', '', $extKey) . '_owlslider';
+
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    $pluginSignature,
+    'FILE:EXT:' . $extKey . '/Configuration/FlexForms/flexform_owlslider.xml'
+);
+?>
--- ext_tables.php
+++ ext_tables.php
@@ -4,26 +4,6 @@ if (! defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    $_EXTKEY,
-    'Owlslider',
-    'owlSlider'
-);
-
-$pluginSignature = str_replace('_', '', $_EXTKEY) . '_owlslider';
-
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    $pluginSignature,
-    'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_owlslider.xml'
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
-    'Configuration/TypoScript', 'owlSlider'
-);
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
     'tx_owlslider_domain_model_item',
     'EXT:owl_slider/Resources/Private/Language/locallang_csh_tx_owlslider_domain_model_item.xlf'
